427d0b01ad8ffe853f7304da9ab7f95943fb22bb
inhadeepblue <deepblue@inha.ac.kr>
2021-09-25 16:48:05 +0900

    v0.5 prime number break키워드 추가, 소수가 아닌 경우 성능향상

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-// v0.3
+// v0.5
 public class ReviewDemo01 {
     public static void main(String[] args) {
         // 소수 : 1과 자기자신외에는 나누어 떨어지지 않는 수
@@ -8,8 +8,11 @@ public class ReviewDemo01 {
         boolean isPrime = true;
 
         for(int k=2; k<number; k++){
-            if(number % k == 0) // 입력된 수를 k로 나누었을 때 나머지가 0인 경우. 즉, 약수
-                cnt++;
+            if(number % k == 0) {
+               isPrime = false;
+    	 break; 
+           }
+          //System.out.println(k);
         }
 
         if(isPrime) // isPrime 변수 값이 true면
