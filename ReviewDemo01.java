93556c463a5a9ed48c50cc6100a18491fe91a1ec
inhadeepblue <deepblue@inha.ac.kr>
2021-09-25 16:37:42 +0900

    v0.2 prime number 반복횟수줄임, 주석추가

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-// v0.1
+// v0.2
 public class ReviewDemo01 {
     public static void main(String[] args) {
         // 소수 : 1과 자기자신외에는 나누어 떨어지지 않는 수
@@ -7,12 +7,12 @@ public class ReviewDemo01 {
         int number = sc.nextInt();
         int cnt = 0;
 
-        for(int k=1; k<=number; k++){
+        for(int k=2; k<number; k++){// 반복 횟수 2회 감소
             if(number % k == 0)
                 cnt++;
         }
 
-        if(cnt == 2)
+        if(cnt == 0) // 카운트가 0이 유지되면 true
             System.out.println(number + "은(는) 소수!");
         else
             System.out.println(number + "은(는) 소수가 아닙니다");
